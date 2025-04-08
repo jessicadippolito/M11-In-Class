f2c07923acbfd23514ced43df92870cf2f6bfe4d
jessicadippolito <jessdipp@gmail.com>
2025-04-08 16:21:28 -0400

    complete

--- m11.py
+++ m11.py
@@ -6,7 +6,6 @@ class TestPrime(unittest.TestCase):
         self.assertTrue(is_prime(3))
         self.assertTrue(is_prime(2))
 
-class TestPrime2(unittest.TestCase):
     def test_non_primes(self):
         self.assertFalse(is_prime(4))
         self.assertFalse(is_prime(-4))
