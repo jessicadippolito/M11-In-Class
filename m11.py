9791f35cc0850424852324720f50afa82c36aebd
jessicadippolito <jessdipp@gmail.com>
2025-04-08 16:43:42 -0400

    complete

--- m11.py
+++ m11.py
@@ -8,11 +8,15 @@ class TestPrime(unittest.TestCase):
 
     def test_non_primes(self):
         self.assertFalse(is_prime(4))
+        self.assertFalse(is_prime(13*17))
+
+    def test_too_small(self):
         self.assertFalse(is_prime(-2))
         self.assertFalse(is_prime(1))
+        self.assertFalse(is_prime(0))
 
 class TestRemove(unittest.TestCase):
     def test_removes(self):
         self.assertEqual(remove_vowels("aeiou"), "")
         self.assertEqual(remove_vowels("AEIOU"), "")
-        self.assertEqual(remove_vowels("cccaeiou"), "ccc")
+        self.assertEqual(remove_vowels("abcdefghijklmnopqrstuvwxyz"), "bcdfghjklmnpqrstvwxyz")
