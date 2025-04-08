41e7872d90e39e46f0a7f0ec684ad19331b7a857
jessicadippolito <jessdipp@gmail.com>
2025-04-08 16:19:57 -0400

    complete

--- m11.py
+++ m11.py
@@ -4,10 +4,13 @@ import unittest
 class TestPrime(unittest.TestCase):
     def test_primes(self):
         self.assertTrue(is_prime(3))
+        self.assertTrue(is_prime(2))
 
 class TestPrime2(unittest.TestCase):
     def test_non_primes(self):
         self.assertFalse(is_prime(4))
+        self.assertFalse(is_prime(-4))
+        self.assertFalse(is_prime(1))
 
 class TestRemove(unittest.TestCase):
     def test_removes(self):
